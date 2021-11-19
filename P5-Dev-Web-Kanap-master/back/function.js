40495e297a53b2d59754664d044f4e09675697a0
Othmane <othmane.dahmani.pro@gmail.com>
2021-11-19 16:47:24 +0100

    chargement dossier

---
+++ P5-Dev-Web-Kanap-master/back/function.js
@@ -0,0 +1,15 @@
+const { get } = require("./routes/product");
+
+// Fonction qui va servir a recuperer les données du formilaire dans car
+let users = [];
+
+document.getElementsByClassName("cart__order__form").addEventListenner("submit" , function(){
+   let input = this.getElementByTagName("input");
+get
+}
+);
+
+
+// Function pour recupere les données de l'API
+fetch("http..")
+.then
