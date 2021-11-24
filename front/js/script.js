cfa66b72a50adc2d92847ffe8406c7e9f4fca8de
Othmane <othmane.dahmani.pro@gmail.com>
2021-11-25 00:24:12 +0100

    Reussite ajout element dans pages dacceuil

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,10 @@
 // Cette function sert a recuperer les produit que l'on a sur l'API 
-// Creation de la variable pour y stocker les données
+
 var apiProduct = []; // creation de la variable ou l'on stockera nos données
 
-fetch ("http://localhost:3000/api/products")
-.then(function(res){
+ fetch ("http://localhost:3000/api/products")
+
+    .then(function(res){
       if (res.ok) {
         return res.json(); // si on recois bien la reponse , on la recevra sous format json
       }
@@ -12,21 +13,24 @@ fetch ("http://localhost:3000/api/products")
     .then(function(value) { // on recupere les données de l'api puis on les stocks dans notre tableau , et on verifie avec un console.log
       apiProduct = value;
       console.log(apiProduct);
+affichageProduitAcceuil();
      });
+//Ici on va prendre les elements recupere dans l'API pour les afficer dans la page d'acceuil
 
-// Ici on va prendre les elements recupere dans l'API pour les afficer dans la page d'acceuil
-/*var articles = document.getElementById("test"); 
-articles.innerHTML= 'Salut je suis rajouter via le JS'
-for(var i = 0; i < apiProduct.length ; i ++){
-    articles.innerHTML = apiProduct;
-   
-   console.log();
+
+function affichageProduitAcceuil() {
+ for (var i = 0; i < apiProduct.length ; i++) {
+    var articles = document.getElementById("items");
+    console.log(apiProduct[i]);
+
+  var test = '' ;
+    test += '<a href="./product.html?id='+apiProduct[i]._id+'"> <article>';  
+    test += '<img src="'+apiProduct[i].imageUrl+'"/>';  
+    test += apiProduct[i].price+' $ ';  
+    test += '</article> </a>';  
+    articles.innerHTML += test;  
+    console.log('i:'+i)
+  }
 }
-*/
-const newElt = document.createElement("article");
-let elt = document.getElementById("items");
-
-elt.appendChild(newElt);
-
 
 
