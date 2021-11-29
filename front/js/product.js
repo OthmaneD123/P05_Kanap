4af3d725eb0800e8f0eb366d65356e92880108e8
Othmane <Othmane.dahmani.pro@gmail.xn--cp-pka>
2021-11-29 21:25:02 +0100

    etape 5 du projet

--- front/js/product.js
+++ front/js/product.js
@@ -1,27 +1,30 @@
-// Variables
-let  title = document.getElementById('title');
-let  price = document.getElementById('price');
-let  description = document.getElementById('description');
-let  colors = document.getElementById('colors');
-let  quantity = document.getElementById('quantity');
-
 // Recuperation de L'id dans l'url
 var str = "http://localhost:3000/api/products";
 var url = new URL(str);
-var name = url.searchParams.get("name");
-console.log(name);
+var idProduct = url.searchParams.get("id");
+console.log(idProduct);
 
 //  Récupérer l’id du produit à afficher
-var idPProduit = 'http://localhost:3000/api/products/' + id;
+var recupId = 'http://localhost:3000/api/products/' + idProduct;
     
-
-iProduit();
+// Récupération des articles de l'API
+function recupArticle(){
+    fetch('http://localhost:3000/api/products/' + idProduct)
+    .then((res) => {
+        return res.json()
+    })
+}
+//idProduit();
 
 //Affichage du produit 
-function afficheProduitProduct () {
+/*function afficheProduitProduct () {
+    let  title = document.getElementById('title');
+    let  price = document.getElementById('price');
+    let  description = document.getElementById('description');
+    let  colors = document.getElementById('colors');
+    let  quantity = document.getElementById('quantity');
+
     title.innerhtml = apiProduct.name;
     price.innerhtml = apiProduct.price;
     description.innerhtml = apiProduct.description;
-} 
-afficheProduitProduct();
-
+} ;*/
--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,20 @@
+// Cette function sert a recuperer les produit que l'on a sur l'API 
 
+var apiProduct = [] ; // creation de la variable ou l'on stockera nos données
+const recupApi = fetch ("http://localhost:3000/api/products")
+.then(function(res){
+      if (res.ok) {
+        return res.json(); // si on recois bien la reponse , on la recevra sous format json
+      }
+      var apiProduct = [] ; // creation de la variable ou l'on stockera nos données
+    })
+
+    .then(function(value) { // on recupere les données de l'api puis on les stocks dans notre tableau , et on verifie avec un console.log
+      apiProduct = value;
+      console.log(apiProduct);
+      affichageProduit();
+     });
+     
 //Ici on va prendre les elements recupere dans l'API pour les afficer dans la page d'acceuil
 
 function affichageProduit() {
@@ -18,4 +34,3 @@ function affichageProduit() {
      console.log('i:'+i)
    }
  }
- affichageProduit()
